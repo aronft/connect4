e2bd556e16f1dcf51ca2892c42d72cccc050eb5c
Aron <blazte.aster@gmail.com>
2024-04-04 20:19:04 -0500

    extend color gray to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+const colors = require('tailwindcss/colors')
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    colors: {
-      purple: {
-        600: "#7C34FB",
-        800: "#5F18D2",
-      },
-      orange: {
-        200: "#ffd06e",
-      },
-      pink: {
-        400: "#fe6687",
-      },
-      white: "#000",
-      black: "#fff",
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            boxShadow: {
+                '3xl': '0 5px 0 0 rgba(0, 0, 0, 1)',
+                '4xl': '0 10px 0 0 rgba(0, 0, 0, 1)',
+            },
+        },
+        colors: {
+            gray: colors.gray,
+            purple: {
+                600: '#7C34FB',
+                800: '#5F18D2',
+            },
+            orange: {
+                200: '#ffd06e',
+            },
+            pink: {
+                400: '#fe6687',
+            },
+            white: '#fff',
+            black: '#000',
+        },
     },
-  },
-  plugins: [],
-};
-export default config;
+    plugins: [],
+}
+export default config
