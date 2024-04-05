9c949123b8f5d3eb4547ab7b1670177490fe545c
Aron <blazte.aster@gmail.com>
2024-04-05 17:47:32 -0500

    Game header (#3)
    * create helper for merge tailwind
    
    * add printWidth to prettier config
    
    * extend color gray to tailwind config
    
    * remove width and height default
    
    * create player icon component
    
    * cretae home page with options and rules
    
    * mend
    
    add logo
    
    * create rules page
    
    * change attributes to jsx
    
    * create card component
    
    * use card component to replace the home option and section in rules page
    
    * create button component
    
    * create Container component
    
    * create header layout component

---
+++ src/features/core/components/ui/objects/container.tsx
@@ -0,0 +1,23 @@
+import { cn } from '@/features/core/utils/class-names'
+
+interface ContainerProps {
+    className?: string
+    children: React.ReactNode
+    as?: keyof React.ReactHTML
+}
+export const Container = ({
+    className,
+    as,
+    children,
+    ...props
+}: ContainerProps) => {
+    const Component = as ?? 'div'
+    return (
+        <Component
+            className={cn('mx-auto max-w-2xl px-2', className)}
+            {...props}
+        >
+            {children}
+        </Component>
+    )
+}
