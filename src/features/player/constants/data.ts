3b37db704008f382f91ff5f14e4adde650a7406b
Aron <blazte.aster@gmail.com>
2024-04-06 17:34:45 -0500

    Create PlayerCard component with avatar variations

---
+++ src/features/player/constants/data.ts
@@ -0,0 +1,9 @@
+import cpu from '@/assets/cpu.svg'
+import player1 from '@/assets/player-one.svg'
+import player2 from '@/assets/player-two.svg'
+
+export const PLAYER_AVATARS = {
+    cpu,
+    player1,
+    player2,
+}
